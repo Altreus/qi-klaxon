94e37375a911bedd21c2c472bb90446a81e74f34
Alastair McGowan-Douglas <alastair.douglas@gmail.com>
2013-09-10 19:22:03 +0100

    LOL KLAXON

---
+++ klaxon.js
@@ -0,0 +1,25 @@
+function hide(el) {
+    return function() {
+        el.style.display = 'none';
+    }
+}
+function show(el) {
+    return function() {
+        el.style.display = 'block';
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    var text = window.location.search.substr(1).replace(/\+/, ' ');
+    var textDiv = document.getElementById('text');
+    
+    textDiv.innerText = text;
+    document.getElementById('klaxon').play();
+
+    setTimeout(hide(textDiv), 500);
+    setTimeout(show(textDiv), 1000);
+    setTimeout(hide(textDiv), 1400);
+    setTimeout(show(textDiv), 1900);
+    setTimeout(hide(textDiv), 2600);
+    setTimeout(show(textDiv), 3300);
+    setTimeout(hide(textDiv), 5000);
+});
